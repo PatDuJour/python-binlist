6c803aa9e6a04c795f6907d14504023c212a55ce
Patrick Zhang <patrick@ticketguardian.net>
2019-02-25 13:07:50 -0800

    import modules to top level

--- binlist/__init__.py
+++ binlist/__init__.py
@@ -5,3 +5,11 @@
 __author__ = """Patrick Zhang"""
 __email__ = 'patdujour@gmail.com'
 __version__ = '0.1.0'
+
+from binlist.iin import BIN
+from binlist import network
+
+__all__ = [
+    'BIN',
+    'network',
+]
